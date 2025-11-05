b4eb124667d73de5902f23e7b69d8b0871f9856d
Nube28 <159978169+Nube28@users.noreply.github.com>
2025-11-04 19:21:05 -0700

    ya funciona chavalada!!

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import { HeaderComponent } from "./header/header";
-import { ContentComponents } from "./content/content";
-import { FooterComponent } from "./footer/footer";
+import { HeaderComponent } from "./header/header.js";
+import { ContentComponents } from "./content/content.js";
+import { FooterComponent } from "./footer/footer.js";
 
 window.customElements.define('header-info', HeaderComponent);
 window.customElements.define('content-info', ContentComponents);
